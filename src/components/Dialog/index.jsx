b89126dd0b9c4ec584f0300e9661aa36a1a40fab
Adriano Cordeiro <adrianocordeiro99@outlook.com>
2026-01-12 14:17:36 -0300

    oOnfigura useEffect, estiliza, e cria componentes reaproveitáveis

--- src/components/Dialog/index.jsx
+++ src/components/Dialog/index.jsx
@@ -1,11 +1,40 @@
-export function Dialog() {
+import React, { useEffect, useRef } from "react";
+import "./dialog.style.css";
+import { IconClose } from "../icons";
+
+export function Dialog({ isOpen, onClose, children }) {
+  //   const dialog = document.querySelector("dialog");
+
+  const dialogRef = useRef(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      openDialog();
+    } else {
+      closeDialog();
+    }
+  }, [isOpen]);
+
+  // "Show the dialog" button opens the dialog modally
+  const openDialog = () => {
+    dialogRef.current.showModal();
+  };
+
+  // "Close" button closes the dialog
+
+  const closeDialog = () => {
+    dialogRef.current.close();
+  };
   return (
     <>
-      <dialog>
-        <button autofocus>Close</button>
-        <p>This modal dialog has a groovy backdrop!</p>
+      <dialog ref={dialogRef} className="dialog">
+        <div className="btn-close-wrapper">
+          <button autoFocus onClick={onClose} className="btn-close">
+            <IconClose />
+          </button>
+        </div>
+        <div className="body">{children}</div>
       </dialog>
-      <button>Show the dialog</button>
     </>
   );
 }
